9d27a48445eb21d70453dc9cd3f99667e4b713f9
Kunhui Pan <nina-khpan@outlook.com>
2022-09-08 22:07:44 +0100

    completed

--- ps4/ps4a.py
+++ ps4/ps4a.py
@@ -1,6 +1,6 @@
 # Problem Set 4A
-# Name: <your name here>
-# Collaborators:
+# Name: <Kunhui Pan>
+# Collaborators: None
 # Time Spent: x:xx
 
 def get_permutations(sequence):
@@ -16,14 +16,20 @@ def get_permutations(sequence):
     Returns: a list of all permutations of sequence
 
     Example:
-    >>> get_permutations('abc')
+    # >>> get_permutations('abc')
     ['abc', 'acb', 'bac', 'bca', 'cab', 'cba']
 
     Note: depending on your implementation, you may return the permutations in
     a different order than what is listed here.
     '''
-
-    pass #delete this line and replace with your code here
+    n = len(sequence)
+    results = [] # track and collect result
+    if n == 1: # if the length of sequence equals to 1, just return the list of this sequence.
+        return [sequence]
+    else: # otherwise, divide the sequence into two parts: the first character and the remaining, then get the permutations of the remaining characters, then add them together.
+        for count, value in enumerate(sequence): # get index and element of the sequence at the same time
+            results += [value+item for item in get_permutations(sequence[:count] + sequence[count+1:])]
+        return results
 
 if __name__ == '__main__':
 #    #EXAMPLE
@@ -35,6 +41,22 @@ if __name__ == '__main__':
 #    # Put three example test cases here (for your sanity, limit your inputs
 #    to be three characters or fewer as you will have n! permutations for a 
 #    sequence of length n)
+    example_input1 = "abc"
+    print("Input: ", example_input1)
+    print("Expected Output: ['abc', 'acb', 'bac', 'bca', 'cab', 'cba']")
+    expected_output1 = get_permutations(example_input1)
+    print("Actual Output: ", expected_output1)
+    print("_________________________")
+    example_input2 = "use"
+    print("Input: ", example_input2)
+    print("Expected Output: ['use', 'ues', 'sue', 'seu', 'eus', 'esu']")
+    expected_output2 = get_permutations(example_input2)
+    print("Actual Output: ", expected_output2)
+    print("_________________________")
+    example_input3 = "are"
+    print("Input: ", example_input3)
+    print("Expected Output: ['are', 'are', 'rae', 'bea', 'ear', 'era']")
+    expected_output3 = get_permutations(example_input3)
+    print("Actual Output: ", expected_output3)
 
-    pass #delete this line and replace with your code here
-
+    print("test", get_permutations("aeiou"),len(get_permutations("aeiou")))
